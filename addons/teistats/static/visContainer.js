ddec6925dd690dd52f8cb61da731215cdd443ba9
Alex Benito <abenito@usal.es>
2018-11-08 13:57:37 +0100

    Create basic view in visContainer.js Update package.json and yarn.lock

--- addons/teistats/static/visContainer.js
+++ addons/teistats/static/visContainer.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var d3 = require('d3');
+// var d3tip = require('d3-tip');
 var bootbox = require('bootbox');
 
 
@@ -10,9 +11,11 @@ var VisContainer = function(selector, options) {
     console.log(options.node);
     self.node = options.node
     var url = self.node.urls.api + 'teistats/statistics/';
+    self.axisAdded = false;
     
     self.mixButton = document.getElementById("startstop-button");
     self.resetButton = document.getElementById("reset-button");
+    
 
     
 
@@ -39,7 +42,6 @@ var VisContainer = function(selector, options) {
 	            callback: function(result) {
 	                if(result) {
 	                	stopAction();
-
 	                	fetch(options.node.urls.api + 'teistats/config/reset-statistics/', {
 	                		method: "DELETE"
 	                	}).then(function(response) {
@@ -82,113 +84,110 @@ var VisContainer = function(selector, options) {
     	self.timerId = setInterval(fetchStatistics, 1000 * seconds);
     };
 
-
-    self.svg = d3.select('#vis-svg-container')
+    self.svg = d3.select('#vis-svg-container');
 
     self.generateDistributionData = function(stats) {
     	var data = new Array();
-    	var xpos = 12;
-    	var ypos = 1;
-    	var width = 12;
-    	var height = 12;
+    	var width = 14;
+    	var height = 14;
 
-    	for (var row = 0; row < stats.length; row++) {
-    		data.push(new Array());
-    		for (var column = 0; column < 100; column++) {
+        self.yScale = d3.scaleLinear().domain([0,stats.length - 1]).range([51, self.containerSize[1] - 50]);
+
+    	for (var queryIdx = 0; queryIdx < stats.length; queryIdx++) {
+            if (!self.axisAdded) {
+                console.log('adding axis');
+                var xAxis = d3.axisBottom().scale(self.xScale);
+                self.svg.append("g")
+                    .attr("transform", "translate(0," + self.yScale(queryIdx) + ")")
+                    .call(xAxis)
+                    .append("text")
+                    .text(stats[queryIdx].element)
+                        .attr("fill", "black")
+                        .attr("x", 225)
+                        .attr("text-anchor", "end")
+                        .attr("font-family", "sans-serif")
+                        .attr("font-size", "10px");
+            }
+
+    		for (var percent = 0; percent < 100; percent++) {
     			var value = 0;
-    			if (stats[row].percentages.hasOwnProperty("" + column))
-    				value = stats[row].percentages["" + column]
-    			data[row].push({
-	    			x: xpos,
-	    			y: ypos,
-	    			width: width,
-	    			height: height,
-	    			value: value
-
-	    		});
-	    		xpos += width; 	
+    			if (stats[queryIdx].percentages.hasOwnProperty("" + percent))
+    				value = stats[queryIdx].percentages["" + percent]
+                if (value !== 0) {
+                    data.push({
+                        x: self.xScale(percent),
+                        y: self.yScale(queryIdx),
+                        width: width,
+                        height: height,
+                        percent: percent,
+                        value: value,
+                        queryIdx: queryIdx
+                    });
+                }
     		}
-    		xpos = 12;
-    		ypos += height + 20;
     	}
+        self.axisAdded = true;
     	return data
     }
 
     self.updateVis = function(statsData) {
-    	console.log(statsData);
-    	if (isEmptyDictionary(statsData)) return;
+        
+        if (!statsData || statsData.statistics.length == 0) return;
+
+        console.log(statsData);
+
 
     	var distData = self.generateDistributionData(statsData.statistics);
+
+
+
+    	// distData = d3.nest()
+    	// 			.key(function(d) { return d.queryIdx}).entries(distData);
+
     	console.log(distData);
 
-    	var line = d3.line();
+    	var container = self.svg.select("#baseGrp");
 
-    	var groups = self.svg.append("g").selectAll(".line")
-    		.data(distData)
-    		.enter().append("g");
-
-    	var lines = groups
-				.append("path")
-    			.attr("d", function(d) { 
-    				return line([[
-		    				d[0].x -12,
-		    				d[0].y 
-		    		],[
-		    				d[d.length - 1].x -12,
-		    				d[d.length - 1].y 
-		    		]]);
-    			})
-    			.attr("stroke", "grey");
-
-		groups.each(function(d, i) {
-			console.log(d);
-			d3.select(this).selectAll("circle")
-						.data(d)
-					.enter()
-						.append("circle")
-						.attr("cx", function(d) { return d.x; })
-						.attr("cy", function(d) { return d.y; })
-						.attr("r", function(d) { return d.value > 0 ? 5 : 0 })
-						.attr("fill", function(d) { return "black"; });
-		});
+    	var circle = container.selectAll("circle").data(distData, function (d) { return "" + d.queryIdx + d.percent; });
 
 
+		var radiusScale = d3.scaleLinear()
+						    .domain(d3.extent(distData, function(a) { return +a.value; }))
+						    .range([2, 15]);
+
+    	circle.enter().append("circle")
+				.attr("cx", function(d) { return d.x; })
+				.attr("cy", function(d) { return d.y; })
+				.attr("r", function(d) { return radiusScale(d.value); })
+				.attr("fill", 'rgba(0, 0, 0, 0.5)')
+			.merge(circle)
+					.transition()
+					.duration(750)
+	    			.ease(d3.easeLinear)
+						.attr("r", function(d) { return radiusScale(d.value); });
+
+		circle.exit().remove();
 
 
-
-    	// var column = row.selectAll(".square")
-    	// 	.data(function(d) { return d; })
-    	// 	.enter().append("rect")
-    	// 	.attr("class", "square")
-    	// 	.attr("x", function(d) { return d.x; })
-    	// 	.attr("y", function(d) { return d.y; })
-    	// 	.attr("width", function(d) { return d.width; })
-    	// 	.attr("height", function(d) { return d.height; })
-    	// 	.style("fill", "#fff")
-    	// 	.style("stroke", "#222");
-
-
-    	// var row = self.svg.selectAll(".row")
-    	// 	.data(rowData)
-    	// 	.enter().append("g")
-    	// 	.attr("class", "row")
-	    // 		.append("rect")
-	    // 		.attr("class", "square")
-	    // 		.attr("x", function(d) { return 200;})
-	    // 		.attr("y", function(d, i) { return 10 * i})
-	    // 		.attr("width", 800)
-	    // 		.attr("height", 10)
-	    // 		.style("fill", function(d) {
-	    // 			if (isEmptyDictionary(d)) {
-		   //  			return "#fff";
-		   //  		} else {
-		   //  			return "#000";
-		   //  		}
-	    // 		})
-	    // 		.style("stroke", "#222");
     };
 
-    self.updateVis([]);
+    window.onload = function () { 
+
+
+        var clientRect = document.getElementById('vis-svg-container').getBoundingClientRect();
+        self.containerSize = [clientRect.right - clientRect.left, clientRect.bottom - clientRect.top];
+        
+
+        self.xScale = d3.scaleLinear().domain([0,100]).range([251, self.containerSize[0] - 200]);
+        // console.log(self.xScale(50));
+
+        self.svg.append("g")
+                .attr("id", "baseGrp");
+
+        
+
+        self.updateVis()
+    }
 };
 
 
